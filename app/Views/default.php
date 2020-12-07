757f58890763c22b45d4a48abe805ead67cc1643
student <student@DESKTOP-L3ET5DL>
2020-12-07 19:58:34 +0200

    default layout

---
+++ app/Views/default.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Codeigniter 4</title>
+</head>
+<body>
+<h1>CodeIgniter 4 Demo</h1>
+    <?= $this->renderSection('content') ?>
+</body>
+</html>
